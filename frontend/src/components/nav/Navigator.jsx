bc378d997049135fac648677e73c1a0c6d96b29d
Hong Seung-Gi <tmdrl5661@naver.com>
2022-01-19 15:40:16 +0900

    #S06P12A507-30/[Add] 로그인 상태 별 네비게이션 바 항목 설정

--- frontend/src/components/nav/Navigator.jsx
+++ frontend/src/components/nav/Navigator.jsx
@@ -1,11 +1,29 @@
 import React,{ useEffect, useState } from 'react';
 import {Link} from 'react-router-dom'
 function Navigator(props) {
-    return(
-      <nav><Link to = "/" >Logo</Link>
-      <Link to = "/login" >로그인</Link></nav>
-    )
-    return
+  const {status, isLogin} = props;
+
+  const handleLogOut = () =>{
+      isLogin("1");
+  }
+  switch(status){
+    case "1" :
+      return(
+        <nav><Link to = "/" >Logo</Link>
+        <Link to = "/login" >로그인</Link></nav>
+      )
+    case "2" :
+      return(
+        <nav><Link to = "/" >Logo</Link>
+        <Link to = "/profile">프로필</Link>
+        <p onClick={handleLogOut}>로그아웃</p></nav>
+      )
+    case "3" :
+      return(
+        <nav><Link to = "/" >Logo</Link>
+        <Link to = "/conferences" >exit</Link></nav>
+      )
+  }
 }
 
 export default Navigator;
