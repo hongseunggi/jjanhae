2f61b63aca2ef15ba1f3d6f4fc88dbee151c3472
soyeon <dtd1170@naver.com>
2022-01-20 03:59:54 +0900

    #S06P12A507-62 [Update] 회원가입 중복여부 체크로직 추가

--- backend/src/main/java/com/ssafy/api/controller/UserController.java
+++ backend/src/main/java/com/ssafy/api/controller/UserController.java
@@ -56,9 +56,14 @@ public class UserController {
 
 		//임의로 리턴된 User 인스턴스. 현재 코드는 회원 가입 성공 여부만 판단하기 때문에 굳이 Insert 된 유저 정보를 응답하지 않음.
 
-		System.out.println("auth_yn : "+registerInfo.getAuthYn());
+		System.out.println("=========== 회원가입 ===========\n");
+		System.out.println("인증 여부 : " + registerInfo.getAuthYn());
+		String userId = userService.getUserByUserId(registerInfo.getUserId()).getUserId();
+		if(!"".equals(userId)){
+			return ResponseEntity.status(409).body(BaseResponseBody.of(409, "이미 존재하는 사용자 ID"));
+		}
 		User user = userService.createUser(registerInfo);
-		System.out.println("register : "+user.getUserId());
+		System.out.println("userId : "+user.getUserId());
 
 		return ResponseEntity.status(200).body(BaseResponseBody.of(200, "Success"));
 	}
