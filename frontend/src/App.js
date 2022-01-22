997c9086f4bccede52ac39b69c34efcd91e8b296
thdalstn6352 <thdalstn6352@naver.com>
2022-01-22 17:47:55 +0900

    [FIx] 코드 에러 수정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,11 @@ import Login from "./components/Login/Login.jsx";
 import Navigator from "./components/nav/Navigator";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React, { useState } from "react";
-<<<<<<< HEAD
-import Login from "./components/Login";
-import Register from "./components/Regist/Register";
-=======
 import Register from "./components/Regist/Register";
 import FindId from "./components/FindId/FindId";
 import FindPwd from "./components/FindPwd/FindPwd";
 import RegisterComplete from "./components/Regist/RegisterComplete";
 import ResetPwd from "./components/FindPwd/ResetPwd";
->>>>>>> 00f977fda5edb38fd1342d8ed67b4b4897e55064
 function App() {
   const [code, setCode] = useState("1");
   const handleIsLogin = (data) => {
@@ -25,19 +20,13 @@ function App() {
       <Router>
         <Navigator status={code} isLogin={handleIsLogin} />
         <Routes>
-<<<<<<< HEAD
+          {/* <Route path="/" element={<ResetPwd />} /> */}
           <Route path="/" element={<Main status={code} />} />
-          <Route path="/login" element={<Login isLogin={handleIsLogin} />} />
-          <Route path="/signup" element={<Register />} />
-=======
-          <Route path="/" element={<ResetPwd />} />
-          {/* <Route path="/" element={<Main status={code} />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Register />} />
           <Route path="/signup/complete" element={<RegisterComplete />} />
           <Route path="/findId" element={<FindId />} />
-          <Route path="/findPwd" element={<FindPwd />} /> */}
->>>>>>> 00f977fda5edb38fd1342d8ed67b4b4897e55064
+          <Route path="/findPwd" element={<FindPwd />} />
         </Routes>
       </Router>
     </div>
