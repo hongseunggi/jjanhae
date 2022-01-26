e6f04b54efe0c79edab1a8cf8a8c8a1681237f51
soyeon <dtd1170@naver.com>
2022-01-27 07:40:27 +0900

    [Test] 빌드배포

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,4 +1,4 @@
-docker build -t jjanhae .
+DOCKER_BUILDKIT=1 docker build -t jjanhae .
 docker rm jjanhae -f
-docker run -d --rm --name jjanhae jjanhae
+docker run -d -p 8081:8081 --name jjanhae jjanhae
 docker image prune -f
