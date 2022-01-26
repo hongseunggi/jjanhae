a62b09428264452584674a7823e496ea618e9320
soyeon <dtd1170@naver.com>
2022-01-27 07:46:23 +0900

    [Test] log출력

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,4 +1,4 @@
-DOCKER_BUILDKIT=1 docker build -t jjanhae .
+DOCKER_BUILDKIT=1 docker build --progress=plain -t jjanhae .
 docker rm jjanhae -f
 docker run -d -p 8081:8081 --name jjanhae jjanhae
 docker image prune -f
