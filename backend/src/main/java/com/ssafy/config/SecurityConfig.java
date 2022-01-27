3e5a3627c7dcfd18e11ad0093365984c29a954d1
amlwq <bhaeun1105@gmail.com>
2022-01-27 16:16:56 +0900

    SecurityConfig.java에서 WebSecurity 받는 함수 추가

--- backend/src/main/java/com/ssafy/config/SecurityConfig.java
+++ backend/src/main/java/com/ssafy/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -64,4 +65,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     	        	    .anyRequest().permitAll()
                 .and().cors();
     }
+    @Override public void configure(WebSecurity web) throws Exception { web.ignoring().antMatchers( "/static/css/**", "/static/js/**", "/static/img/**"); }
+
 }
