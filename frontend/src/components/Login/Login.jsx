d856469337f09a3e326e7f2e5c8e676e9e0621d2
dkfkqldk97 <dkfkqldk97@naver.com>
2022-01-28 09:36:13 +0900

    S06P12A507-108/[Update] 소셜 로그인 버튼 css수정

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -4,6 +4,8 @@ import styles from "./Login.module.css";
 import { Link } from "react-router-dom";
 import { ReactComponent as IdIcon } from "../../assets/icons/userid.svg";
 import { ReactComponent as PwdIcon } from "../../assets/icons/password.svg";
+import { ReactComponent as KakaoIcon } from "../../assets/icons/kakao.svg";
+import { ReactComponent as GoogleIcon } from "../../assets/icons/google.svg";
 import logo from "../../assets/icons/logo.png";
 import UserApi from "../../api/UserApi.js";
 import LoginStatusContext from "../../contexts/LoginStatusContext";
@@ -198,25 +200,36 @@ const Login = () => {
               <button className={styles.findPwdBtn}>비밀번호 찾기</button>
             </Link>
           </div>
-          <GoogleLoginBtn />
+          <hr className={styles.horizontalLine}/>
+            <div className={styles.socialButton}>
+              <KakaoBtn/>
+              <GoogleBtn/> 
+              {/* <GoogleLoginBtn className={styles.GoogleLogin} /> */}
+            </div>
 
-          {/* <KakaoBtn/>
-            <GoogleBtn/> */}
         </form>
       </div>
     </>
   );
 };
 
-// function KakaoBtn() {
-//     return(
-//         <button>카카오 로그인</button>
-//     )
-// }
+function KakaoBtn() {
+    return(
+      <div>
+        <button className={styles.kakaoBtn}>
+          <KakaoIcon className={styles.icon}/>카카오 로그인
+        </button>
+      </div>
+    )
+}
 
-// function GoogleBtn() {
-//     return(
-//         <button>구글 로그인</button>
-//     )
-// }
+function GoogleBtn() {
+    return(
+      <div>
+      <button className={styles.googleBtn}>
+        <GoogleIcon className={styles.icon}/>구글 로그인
+      </button>
+    </div>
+    )
+}
 export default Login;
