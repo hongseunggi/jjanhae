494661fc45ab692008017fc0ba8bbf91b7e27804
thdalstn6352 <thdalstn6352@naver.com>
2022-02-03 10:21:01 +0900

    #S06P12A507-186/[Add] 아이디찾기, 비밀번호 찾기 UI구현

--- frontend/src/routes/User.js
+++ frontend/src/routes/User.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import CalendarPage from "../components/Calendar/CalendarPage";
-import FindId from "../components/FindId/FindId";
-import FindPwd from "../components/FindPwd/FindPwd";
 import ResetPwd from "../components/FindPwd/ResetPwd";
 import Login from "../components/Login/Login";
 import Profile from "../components/Profile/Profile";
-import Register from "../components/Regist/Register";
-import RegisterComplete from "../components/Regist/RegisterComplete";
+import FindAccount from "../components/Regist/FindAccount";
+import FindPassword from "../components/Regist/FindPassword";
 import RegisterTemplate from "../components/Regist/RegisterTemplate";
 
 const User = () => {
@@ -15,10 +13,9 @@ const User = () => {
     <div>
       <Routes>
         <Route path="/login" element={<Login />} />
-        {/* <Route path="/signup" element={<Register />} /> */}
         <Route path="/signup/*" element={<RegisterTemplate />} />
-        <Route path="/findid" element={<FindId />} />
-        <Route path="/findpwd" element={<FindPwd />} />
+        <Route path="/findid" element={<FindAccount />} />
+        <Route path="/findpwd" element={<FindPassword />} />
         <Route path="/newpwd" element={<ResetPwd />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/calendar" element={<CalendarPage />} />
