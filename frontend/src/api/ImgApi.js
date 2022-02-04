ace89d1dd65f7a1135c1ea47b396bb63864095c4
Hong Seung-Gi <tmdrl5661@naver.com>
2022-02-04 18:55:39 +0900

    [Fix] axios.default.header 세팅 위치 변경

--- frontend/src/api/ImgApi.js
+++ frontend/src/api/ImgApi.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 // const BASE_URL = "http://localhost:8081/api/v1/img";
 const BASE_URL = "https://i6a507.p.ssafy.io/api/v1/img";
-axios.defaults.headers.Authorization = "Bearer "+sessionStorage.getItem("accessToken");
 
 const getImgUploadResult = async (body) => {
     const result = await axios.post(`${BASE_URL}/upload`, body);
