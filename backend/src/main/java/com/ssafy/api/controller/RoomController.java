94e80f0eb2dd6355f0546dfad4c5de553fee35d1
soyeon <dtd1170@naver.com>
2022-02-06 22:02:27 +0900

    [Update] 방관련 api 요청 url 변경

--- backend/src/main/java/com/ssafy/api/controller/RoomController.java
+++ backend/src/main/java/com/ssafy/api/controller/RoomController.java
@@ -36,7 +36,7 @@ import java.time.LocalDateTime;
  */
 @Api(value = "방 API")
 @RestController
-@RequestMapping("/api/v1/conferences")
+@RequestMapping("/api/v1/room")
 public class RoomController {
 
     @Autowired
