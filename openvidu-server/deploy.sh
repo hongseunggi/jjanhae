acc8656f307c09ec93a12b5f99848cbfdd0d6791
amlwq <bhaeun1105@gmail.com>
2022-02-11 02:53:17 +0900

    [Fix] openvidu custom 서버 port 4443 자동 배포 설정

--- openvidu-server/deploy.sh
+++ openvidu-server/deploy.sh
@@ -1,12 +1,13 @@
-echo remove existed home/ubuntu/CustomOpenvidu/openvidu-server
-rm /home/ubuntu/CustomOpenvidu/openvidu-server
+rm -d /home/ubuntu/CustomOpenvidu/openvidu-server
 cp -r ./openvidu-server /home/ubuntu/CustomOpenvidu
-cp /home/ubuntu/i6a507.p.ssafy.io /home/ubuntu/CustomOpenvidu/openvidu-server/src/main/resources
+cp /home/ubuntu/i6a507.p.ssafy.io.p12 /home/ubuntu/CustomOpenvidu/openvidu-server/src/main/resources
 cd /home/ubuntu/CustomOpenvidu/openvidu-server
 mvn clean install -U
 
-DOCKER_BUILDKIT=1 docker build --progress=plain -t openvidu-server .
-docker rm openvidu-server -f
-docker run -d -p 8443:8443 --name openvidu-server openvidu-server
+docker stop openvidu-server-custom
+DOCKER_BUILDKIT=1 docker build --progress=plain -t openvidu/openvidu-server .
+docker rm openvidu-server-custom -f
+docker run -d -p 4443:4443 --name openvidu-server-custom openvidu/openvidu-server
+docker cp target/openvidu-*.jar openvidu-server-custom:/
 #docker-compose -f ../gateway/docker-compose.yml up -d --force-recreate --no-deps api
 docker image prune -f
