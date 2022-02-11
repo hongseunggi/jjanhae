8d1e1ef169d83efb4cd972526eabe27a7b2d0ee3
amlwq <bhaeun1105@gmail.com>
2022-02-11 17:08:04 +0900

    fix deploy.sh

--- openvidu-server/deploy.sh
+++ openvidu-server/deploy.sh
@@ -1,6 +1,7 @@
 sudo rm -rf /home/ubuntu/CustomOpenvidu/openvidu/openvidu-server
 cd /home/ubuntu/CustomOpenvidu
 mkdir openvidu-server
+cd /home/ubuntu/CustomOpenvidu/openvidu/openvidu-server
 sudo cp -r /home/jenkins/workspace/jjanhae-dev-openvidu-server-build/openvidu-server ./
 sudo cp /home/ubuntu/i6a507.p.ssafy.io.p12 /home/ubuntu/CustomOpenvidu/openvidu/openvidu-server/src/main/resources
 
@@ -12,5 +13,5 @@ sudo chmod +x .create_image.sh
 ./create_image.sh 2.20.1
 
 cd /home/ubuntu/opt/openvidu
-./openvidu restart
+./openvidu restart &
 docker image prune -f
