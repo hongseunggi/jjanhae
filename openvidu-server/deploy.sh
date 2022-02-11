b79fe4ea811af3510df3de852314dd2783d9e617
amlwq <bhaeun1105@gmail.com>
2022-02-12 08:54:39 +0900

    [Fix] deploy.sh

--- openvidu-server/deploy.sh
+++ openvidu-server/deploy.sh
@@ -16,17 +16,3 @@ mvn clean install -U
 echo jjanhae - build maven openvidu
 cd /home/ubuntu/CustomOpenvidu/openvidu
 mvn package -DskipTests
-echo jjanhae - move to docker
-cd /home/ubuntu/CustomOpenvidu/openvidu/openvidu-server/docker/openvidu-server
-echo jjanhae - run create_image.sh
-sudo chmod +x create_image.sh
-sudo su
-./create_image.sh 2.20.1
-ehco jjanhae - stop jenkins
-cd /home/ubuntu
-
-#su ubuntu
-#cd /home/ubuntu/opt/openvidu
-#미치겠네 이거 매번 다시켜야하나....
-#nohup ./openvidu restart & > /dev/null
-docker image prune -f
