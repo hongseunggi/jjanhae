a5a8af09de82bb00ca1500ac2ca2331eb58c84f6
amlwq <bhaeun1105@gmail.com>
2022-02-12 12:03:42 +0900

    [Update] 방 입장시 비밀번호 입력 API 추가하고 방 입력할 땐 roomSeq만 받고 비밀번호 검사 안함  / 신청곡 삭제 개발중

--- openvidu-server/src/main/java/io/openvidu/server/contents/MusicService.java
+++ openvidu-server/src/main/java/io/openvidu/server/contents/MusicService.java
@@ -14,6 +14,17 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class MusicService {
 
+    /** 음악 재생 */
+    static final int PLAYMUSIC = 1;
+    /** 음악 일시정지 */
+    static final int PAUSEMUSIC = 2;
+    /** 음악 중지 */
+    static final int STOPMUSIC = 3;
+    /** 음악 추가 */
+    static final int ADDMUSIC = 4;
+    /** 음악 삭제 */
+    static final int DELETEMUSIC = 5;
+
     static RpcNotificationService rpcNotificationService;
 
      // < sessionId, 노래목록(비디오아이디 여러개) >
@@ -38,50 +49,109 @@ public class MusicService {
         String dataString = message.get("data").getAsString();
         JsonObject data = (JsonObject) JsonParser.parseString(dataString);
 
-        // 세션 아이디 저장
-        String sessionId = data.get("sessionId").getAsString();
-
         // 입력받은 값들 출력
-        System.out.println("음악 data : " + data); //{"videoId":"5uk6cFPL19w","nickname":"OpenVidu_User43","streamId":"str_CAM_DM92_con_ZIlIJZTwJ8"}
-        System.out.println("음악 params : " + params); // {"from":"con_AG3mOjqFdT","type":"signal:music"}
+        System.out.println("[Music] 받은 음악 data : " + data); //{"videoId":"5uk6cFPL19w","nickname":"OpenVidu_User43","streamId":"str_CAM_DM92_con_ZIlIJZTwJ8"}
+        System.out.println("[Music] 받은 음악 params : " + params); // {"from":"con_AG3mOjqFdT","type":"signal:music"}
 
-        // 노래 상태 변경 : 재생 , 일시정지 , 멈춤, 다음곡
-        if (data.has("isPlaying")){
-            String isPlaying = data.get("isPlaying").getAsString();
-            System.out.println("바꿔줬으면 하는 노래 진행 상태 : " + isPlaying);
-            if (Objects.equals(isPlaying, "next")) {
-                requestSongsMap.get(sessionId).remove(0); // 리스트의 제일 앞의 노래를 제거
-//                data.addProperty("songList", requestSongsMap.get(sessionId));
-            }
+        // 원하는 상태에 따른 수행 방식 변경
+        int musicStatus = data.get("musicStatus").getAsInt();
 
-            params.addProperty("data", data.toString());
-            // 브로드 캐스팅
-            for (Participant p : participants) {
-                rpcNotificationService.sendNotification(p.getParticipantPrivateId(),
-                        ProtocolElements.PARTICIPANTSENDMESSAGE_METHOD, params);
-                System.out.println("노래상태 반환 " + params);
-            }
-
-        }
-
-        // 음악 추가
-        if (data.has("videoId")){
-            String videoId = data.get("videoId").getAsString();
-            System.out.println("이 비디오 아이디를 뿌려주세요 : " + videoId);
-            requestSongsMap.get(sessionId).add(videoId);
-            data.addProperty("songList", requestSongsMap.get(sessionId).toString());
-            System.out.println(data);
-
-            params.addProperty("data", data.toString());
-            for (Participant p : participants) {
-                rpcNotificationService.sendNotification(p.getParticipantPrivateId(),
-                        ProtocolElements.PARTICIPANTSENDMESSAGE_METHOD, params);
-                System.out.println("음악 추가 반환 내용 : " + params);
-            }
-
+        switch (musicStatus) {
+            case PLAYMUSIC: // 음악 재생
+            case PAUSEMUSIC: // 음악 일시정지
+            case STOPMUSIC: // 음악 멈춤
+                sendStatus(musicStatus, participant, message, participants, params, data);
+                return;
+            case ADDMUSIC: // 음악 추가
+                addMusic(participant, message, participants, params, data);
+                return;
+            case DELETEMUSIC: // 음악 삭제
+                delMusic(participant, message, participants, params, data);
+                return;
         }
 
     }
 
+
+    /**
+     * 음악 상태 그대로 돌려줌
+     * musicStatus: 1, 2, 3
+     * */
+    private void sendStatus(int musicStatus, Participant participant, JsonObject message, Set<Participant> participants,
+                        JsonObject params, JsonObject data) {
+        System.out.println("[Music] 바꿔줬으면 하는 노래 진행 상태 : " + musicStatus);
+        params.add("data", data);
+        // 브로드 캐스팅
+        for (Participant p : participants) {
+            rpcNotificationService.sendNotification(p.getParticipantPrivateId(),
+                    ProtocolElements.PARTICIPANTSENDMESSAGE_METHOD, params); // sendMessage
+        }
+        System.out.println("[Music] 노래상태 반환 " + params);
+    }
+
+    /**
+     * 음악 추가
+     * musicStatus: 4
+     * */
+    private void addMusic(Participant participant, JsonObject message, Set<Participant> participants,
+                            JsonObject params, JsonObject data) {
+        String sessionId = data.get("sessionId").getAsString();
+        String videoId = data.get("videoId").getAsString();
+        System.out.println("[Music] 추가하고 싶은 노래(비디오아이디) : " + videoId);
+        // 해당 세션에 아이디 추가
+        // 만약 첫 신청곡이라면 list가 없으므로 list 추가
+
+        ArrayList<String> songList = new ArrayList<>();
+        // 비어있지 않다면 기존 노래 리스트로 변경
+        if (requestSongsMap.get(sessionId) != null){
+            songList = requestSongsMap.get(sessionId);
+        }
+        songList.add(videoId);
+        requestSongsMap.put(sessionId, songList);
+        data.addProperty("songList", requestSongsMap.get(sessionId).toString());
+        // {"sessionId":"SessionA","videoId":"ㅁㄴㅇㄴㅇㄻㄴㅇㄹ","nickname":"OpenVidu_User20","streamId":"str_CAM_ZuID_con_EbPKJ0cczg","songList":"[ㄱㅁㄷㄹ, ㄴㅇㄻㄴㅇㄹ, ㅁㄴㅇㄴㅇㄻㄴㅇㄹ]"}
+
+        params.add("data", data);
+        for (Participant p : participants) {
+            rpcNotificationService.sendNotification(p.getParticipantPrivateId(),
+                    ProtocolElements.PARTICIPANTSENDMESSAGE_METHOD, params);
+            System.out.println("[Music] 음악 추가 반환 내용 : " + params);
+        }
+    }
+
+    /**
+     * 음악 삭제
+     * musicStatus: 5
+     * */
+    private void delMusic(Participant participant, JsonObject message, Set<Participant> participants,
+                            JsonObject params, JsonObject data) {
+        String sessionId = data.get("sessionId").getAsString();
+        String videoId = data.get("videoId").getAsString();
+        System.out.println("[Music] 삭제하고 싶은 노래(비디오아이디) : " + videoId);
+
+        ArrayList<String> songList = requestSongsMap.get(sessionId);
+        Integer removeIdx = songList.indexOf(videoId);
+        System.out.println("지금 노래 리스트" + songList);
+        if (removeIdx == -1) {
+            System.out.println("[Music] 삭제하려는 노래가 없습니다.");
+            return;
+        }
+        songList.remove(removeIdx);
+        System.out.println("바뀐 노래 리스트" + songList);
+        System.out.println("현재 requestSongMap" + requestSongsMap);
+        requestSongsMap.remove(sessionId);
+        System.out.println("노래 삭제한 requestSongMap" + requestSongsMap);
+        requestSongsMap.put(sessionId, songList);
+        System.out.println("바뀐 requestSongMap" + requestSongsMap);
+        data.addProperty("songList", requestSongsMap.get(sessionId).toString());
+
+        params.add("data", data);
+        // 브로드 캐스팅
+        for (Participant p : participants) {
+            rpcNotificationService.sendNotification(p.getParticipantPrivateId(),
+                    ProtocolElements.PARTICIPANTSENDMESSAGE_METHOD, params); // sendMessage
+        }
+        System.out.println("[Music] 음악 삭제 반환 내용 " + params);
+    }
 
 }
