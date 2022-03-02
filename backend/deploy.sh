4837405a023fca7e87182ed46a011cea0f274d72
Hong Seung-Gi <tmdrl5661@naver.com>
2022-03-03 02:30:43 +0900

    [Update] 쉘 스크립트

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,5 +1,5 @@
 DOCKER_BUILDKIT=1 docker build --progress=plain -t jjanhae .
 #docker rm jjanhae -f
 #docker run -d -p 8081:8081 --name jjanhae jjanhae
-docker-compose -f ../gateway/docker-compose.yml up -d --force-recreate --no-deps api
+docker-compose -f /jenkins/workspace/backend-buil/gateway/docker-compose.yml up -d --force-recreate --no-deps api
 docker image prune -f
