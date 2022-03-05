9aa990427b6fd97b90e3bb1d1ed0058f2e2ef897
Hong Seung-Gi <tmdrl5661@naver.com>
2022-03-05 20:10:50 +0900

    [Update] openvidu

--- openvidu-server/deploy.sh
+++ openvidu-server/deploy.sh
@@ -5,7 +5,7 @@ cd /home/ubuntu/CustomOpenvidu/openvidu
 echo jjanhae - create new openvidu-server directory
 mkdir openvidu-server
 echo jjanhae - copy openvidu-server
-sudo cp -r /home/jenkins/workspace/jjanhae-dev-openvidu-server-build/openvidu-server ./
+sudo cp -r /jenkins/workspace/jjanhae-dev-openvidu-server-build/openvidu-server ./
 echo jjanhae - copy key
 sudo cp /home/ubuntu/jjanhae.p12 /home/ubuntu/CustomOpenvidu/openvidu/openvidu-server/src/main/resources
 
