ed0301e0ab88a9e620711404cab25ba94a880ef6
Hong Seung-Gi <tmdrl5661@naver.com>
2022-03-05 19:47:04 +0900

    [Update] deploy

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,6 +1,6 @@
 sudo cp /home/ubuntu/jjanhae.p12 /jenkins/workspace/backend-buil/backend
 DOCKER_BUILDKIT=1 docker build --no-cache --progress=plain -t jjanhae .
-# docker rm jjanhae -f
-# docker run -d -p 8081:8081 --name jjanhae jjanhae
-docker-compose -f /jenkins/workspace/backend-buil/gateway/docker-compose.yml up -d --force-recreate --no-deps api
+docker rm jjanhae -f
+docker run -d -p 8081:8081 --name jjanhae jjanhae
+# docker-compose -f /jenkins/workspace/backend-buil/gateway/docker-compose.yml up -d --force-recreate --no-deps api
 docker image prune -f
